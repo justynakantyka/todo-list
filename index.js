3550c959a41ce6d61c6cc3906047c95712e0568b
J K <just.kantyka@gmail.com>
2023-08-24 15:23:19 +0200

    Change css styles. Add post methods for private work to-do lists

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+const privateTaskList = [];
+const workTaskList = [];
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("private.ejs", {privateTaskList: privateTaskList});
 });
 
 app.get("/work", (req, res) => {
-    res.render("index.ejs");
+    res.render("work.ejs", {workTaskList: workTaskList});
+})
+
+app.post("/", (req, res) => {
+    privateTaskList.push(req.body["task"]);
+    res.render("private.ejs", {privateTaskList: privateTaskList});
+})
+
+app.post("/work", (req, res) => {
+    workTaskList.push(req.body["task"]);
+    res.render("work.ejs", {workTaskList: workTaskList});
 })
 
 app.listen(port, () => {
