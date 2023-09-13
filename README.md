2b09d46c36dc637e3fdedb1844980f9a68153764
J K <just.kantyka@gmail.com>
2023-09-13 22:53:03 +0200

    Add today and work todo lists
    - Add navigation for today and work lists
    - Add option for removing from lists

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # todo-list
 Simple and intuitive tool designed to help you manage your tasks efficiently, whether they are related to your private life or work responsibilities.
+
+## Requirements
+- Node.js 18 or Node.js 16 installed (To check node version run: `node -v` or `nvm version` (if you are using nvm))
+- Npm installed (It should already be installed when you install Node.js. To check, run: `npm -v`)
+- MongoDB Installed and Configured
+
 ## Getting Started
 1. Clone the Repository:
 ```
@@ -9,22 +15,31 @@ git@github.com:justynakantyka/todo-list.git
 ```
 npm i
 ```
-3. Run the Application locally:
+3. Run MongoDB server locally using command:
+```
+mongod
+```
+by default, mongodb server will start at port `27017` \
+4. Run the Application locally :
 ```
 node index.js
 ```
-4. Open your web browser and navigate to: http://localhost:3000
+Application is connected to mongodb database: `todolistDB://localhost:27017/todolistDB` \
+5. Open your web browser and navigate to: http://localhost:3000
 
 ## Overview
 
 ### Screenshot
 ![](./screenshots/todoprivate.png)
 
-## My process
-
-### Built with
+## Built with
 * HTML
 * CSS
 * JavaScript
 * EJS
 * Bootstrap
+* Mongoose
+* MongoDB
+
+## To Do
+- [ ] Add to do list for work/home
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
+import _ from 'lodash';
 
 const app = express();
 const port = 3000;
+const defaultListName = "Today";
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -32,48 +34,64 @@ const listSchema = {
 
 const List = mongoose.model("List", listSchema);
 
-
 app.get("/", async (req, res) => {
     const tasks = await Task.find({});
     if(tasks.length === 0) {
         await Task.insertMany(defaultTasks);
         res.redirect("/");
     } else {
-        res.render("index.ejs", {taskList: tasks});
+        res.render("index.ejs", {listTitle: defaultListName, taskList: tasks});
     }
 });
 
+app.get("/favicon.ico", (req,res)=>{
+    return "your favicon";
+})   
+
 app.get("/:taskType", async(req, res) => {
-    const taskType = req.params.taskType;
+    const taskType = _.capitalize(req.params.taskType);
     const tasksList = await List.findOne({name: taskType});
     if(!tasksList){
         const list = new List({
             name: taskType,
             tasks: defaultTasks
         })
-        list.save();
-        console.log('doesnt exist');
+        await list.save();
+        res.redirect("/" + taskType);
     } else {
-        res.render("index.ejs", {listTitle: tasksList.name, newTasks: tasksList.tasks});
+        res.render("index.ejs", {listTitle: tasksList.name, taskList: tasksList.tasks});
     }
   })
 
 app.post("/", async(req, res) => {
     const taskName = req.body.task;
+    const listName = req.body.listTitle;
     const task = new Task({
         name: taskName
     });
-    task.save();
 
-    res.redirect("/");
+    if(listName === defaultListName) {
+        task.save();
+        res.redirect("/");
+    } else {
+        const foundList = await List.findOne({name: listName});
+        foundList.tasks.push(task);
+        foundList.save();
+        res.redirect("/" + listName);
+    }
 });
 
 app.post("/delete", async(req, res) => {
     const checkedTaskId = req.body.checkbox;
+    const listName = req.body.listName;
 
-    await Task.findByIdAndRemove(checkedTaskId);
-
-    res.redirect("/");
+    if (listName === defaultListName) {
+        await Task.findByIdAndRemove(checkedTaskId);
+        res.redirect("/");
+    } else {
+        await List.findOneAndUpdate({name: listName}, { $pull: {tasks: {_id: checkedTaskId}}});
+        res.redirect("/" + listName);
+    }
 });
 
 app.listen(port, () => {
